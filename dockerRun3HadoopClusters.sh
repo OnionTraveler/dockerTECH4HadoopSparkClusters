b13ed227a44a389432c61ee417856b2d89d9cc7a
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-05-04 21:36:13 +0800

    Update dockerRun3HadoopClusters.sh

--- dockerRun3HadoopClusters.sh
+++ dockerRun3HadoopClusters.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[ -e ./myDockerfiles/onionfile ] && echo "The 「onionfile」 has existed" || docker build -f ./myDockerfiles/onionfile -t oniontraveler/hadoop_container:19.5.4 .
+ [ `docker images | grep 'oniontraveler/hadoop_container' | cut -d ' ' -f 1,4 --output-delimiter=':'` ] && echo "The image 「oniontraveler/hadoop_container:19.5.4」 has existed！" || docker build -f ./myDockerfiles/onionfile -t oniontraveler/hadoop_container:19.5.4 .
 
 docker run -itd --name master --hostname master -p 50070:50070 -p 8088:8088 -p 8080:8080 oniontraveler/hadoop_container:19.5.4
 docker run -id --name slaver1 --hostname slaver1 oniontraveler/hadoop_container:19.5.4
