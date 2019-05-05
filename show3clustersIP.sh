b157a2f9cc000150815db8e1bf7645c528ebb6a9
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-05-05 23:50:42 +0800

    Update show3clustersIP.sh

--- show3clustersIP.sh
+++ show3clustersIP.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+
+#========================= docker inspect
  [ -e ./ip-inspect.txt ] && echo "The 「ip-inspect.txt」 file has existed" || touch ./ip-inspect.txt
  [ `cat ./ip-inspect.txt | grep 'master' | cut -d ' ' -f 2` ] && echo "The 「master」 in ip-inspect.txt has existed！" || echo "`docker inspect  --format "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" master` master" >> ./ip-inspect.txt
  [ `cat ./ip-inspect.txt | grep 'slaver1' | cut -d ' ' -f 2` ] && echo "The 「slaver1」 in ip-inspect.txt has existed！" || echo "`docker inspect  --format "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" slaver1` slaver1" >> ./ip-inspect.txt
@@ -7,3 +9,7 @@
 echo " "
 echo =======docker inspect=======
 cat ip-inspect.txt
+
+#========================= (siao sheng miè ji)
+rm -f ./ip-inspect.txt
+
