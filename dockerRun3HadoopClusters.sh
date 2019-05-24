743c0bb7c65a326bc003e5fa11157b29c73ec9ab
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-05-24 18:04:43 +0800

    Update dockerRun3HadoopClusters.sh

--- dockerRun3HadoopClusters.sh
+++ dockerRun3HadoopClusters.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # docker network rm HDSP4onion
  [ `docker network ls | grep 'HDSP4onion' | cut -d ' ' -f 9` ] && echo "The network 「HDSP4onion」 has existed！" || docker network create -d bridge HDSP4onion
 
@@ -10,6 +9,7 @@
  [ `docker ps -a | grep 'master' | rev | cut -d ' ' -f 1 | rev` ] && echo "The container 「master」 has existed" || docker run -itd --name master --hostname master -p 50070:50070 -p 8088:8088 -p 8080:8080 -p 4040:4040 --network=HDSP4onion oniontraveler/hadoop_container:19.5.4
  [ `docker ps -a | grep 'slaver1' | rev | cut -d ' ' -f 1 | rev` ] && echo "The container 「slaver1」 has existed" || docker run -id --name slaver1 --hostname slaver1 -p 50071:50070 --network=HDSP4onion oniontraveler/hadoop_container:19.5.4
  [ `docker ps -a | grep 'slaver2' | rev | cut -d ' ' -f 1 | rev` ] && echo "The container 「slaver2」 has existed" || docker run -td --name slaver2 --hostname slaver2 --network=HDSP4onion oniontraveler/hadoop_container:19.5.4
+
 
 
 #========================= (port explanation)
@@ -24,4 +24,9 @@
 #========================= (docker commands for entering into the container(master or slaver1))
 # docker exec -it master /bin/bash
 # docker exec -it slaver1 /bin/bash
+
+
+#========================= (docker commands for remove all containers)
+# docker stop master slaver1 slaver2
+# docker rm master slaver1 slaver2
 
